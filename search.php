78a7e8fb7c5e707b2cfe794a30a4b676cde159cd
Ian <ian@ian-VirtualBox.(none)>
2014-05-01 12:37:24 -0500

    updating files

--- search.php
+++ search.php
@@ -1,13 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
 
-$string_reddit = file_get_contents("http://www.reddit.com/r/funny/search.json?q=cat");
+echo "hello world"
+
+/*$string_reddit = file_get_contents("http://www.reddit.com/r/funny/search.json?q=cat&restrict_sr=on");
 $json = json_decode($string_reddit, true);  
 
 $children = $json['data']['children'];
 foreach ($children as $child){
     print $child['data']['author'];
-    print $child['data']['title'];
     print "\r\n";
-}
+    print $child['data']['title'];
+    print "\r\n\r\n";
+}*/
 
 ?>
+</body>
+</html>
+
